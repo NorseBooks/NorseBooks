2139b0131d779ed00ee0e8be99409e79a68f7dfd
WKHAllen <wkhallen@gmail.com>
2021-08-01 16:17:21 -0400

    Configure for Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const port = process.env.PORT || 3000;
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`App running on port ${port}`);
 }
 
 bootstrap();
