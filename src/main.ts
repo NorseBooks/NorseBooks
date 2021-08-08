704c113838bd3e4d2b7064f09dc4b0ce3dc861e5
WKHAllen <wkhallen@gmail.com>
2021-08-07 21:31:57 -0400

    Redirect HTTP to HTTPS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as enforce from 'express-sslify';
 
+/**
+ * Debug/production environment.
+ */
+const debug = !!parseInt(process.env.DEBUG);
+
+/**
+ * Port number.
+ */
 const port = process.env.PORT || 3000;
 
+/**
+ * Bootstrap the application.
+ */
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  if (!debug) {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  }
+
   await app.listen(port);
   console.log(`App running on port ${port}`);
 }
