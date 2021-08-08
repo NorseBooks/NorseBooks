7e55d775ecb7c76db2b51bc435da93bc0a814423
WKHAllen <wkhallen@gmail.com>
2021-08-07 22:09:15 -0400

    Add cookie parsing

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
   await app.listen(port);
   console.log(`App running on port ${port}`);
 }
