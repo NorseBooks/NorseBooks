c218d56a1ac4c87ab4263e6621c43ee5aa38c20d
WKHAllen <wkhallen@gmail.com>
2021-08-10 21:19:49 -0400

    Implement user session decorator

---
+++ src/guards/user-session-optional.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { SessionService } from '../services/session/session.service';
+
+@Injectable()
+export class UserSessionOptionalGuard implements CanActivate {
+  constructor(private readonly sessionService: SessionService) {}
+
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const sessionID: string | undefined = request.cookies?.sessionID;
+
+    if (sessionID) {
+      const user = await this.sessionService.getUserBySessionID(sessionID);
+      request.userSession = user;
+    }
+
+    return true;
+  }
+}
