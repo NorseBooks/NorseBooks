17652debd8df37b0949ddc302fe127af1532cd03
WKHAllen <wkhallen@gmail.com>
2021-09-13 22:07:28 -0400

    Add home page book searching

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -38,10 +42,14 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
     MatIconModule,
     MatToolbarModule,
     MatTooltipModule,
--- app/src/app/components/home/home.component.ts
+++ app/src/app/components/home/home.component.ts
@@ -1,7 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user/user.service';
+import { DepartmentService } from '../../services/department/department.service';
 import { BookService } from '../../services/book/book.service';
+import { SearchSortService } from '../../services/search-sort/search-sort.service';
+import { NBDepartment } from '../../services/department/department.interface';
 import { NBBook } from '../../services/book/book.interface';
+import { NBSearchSort } from '../../services/search-sort/search-sort.interface';
+import { inputAppearance } from '../../globals';
+
+/**
+ * The search books form.
+ */
+interface SearchBooksForm {
+  query?: string;
+  departmentID?: number;
+  courseNumber?: number;
+  sortID: number;
+}
 
 /**
  * The app home page.
@@ -13,20 +28,51 @@ import { NBBook } from '../../services/book/book.interface';
 })
 export class HomeComponent implements OnInit {
   public loggedIn = false;
+  public submittingSearch = false;
+  public searchError = '';
+  public defaultSortValue = -1;
+  public departments: NBDepartment[] = [];
+  public sortOptions: NBSearchSort[] = [];
   public searchResults: NBBook[] = [];
   public recommended: NBBook[] = [];
+  public readonly inputAppearance = inputAppearance;
 
   constructor(
     private readonly userService: UserService,
+    private readonly departmentService: DepartmentService,
     private readonly bookService: BookService,
+    private readonly searchSortService: SearchSortService,
   ) {}
 
   public async ngOnInit(): Promise<void> {
     this.loggedIn = this.userService.loggedIn();
+    this.departments = await this.departmentService.getDepartments();
+    this.sortOptions = await this.searchSortService.getSortOptions();
     this.searchResults = await this.bookService.searchBooks({}, 0);
+    this.defaultSortValue = 0;
 
     if (this.loggedIn) {
       this.recommended = await this.userService.getRecommendations();
     }
+  }
+
+  public async onSearchBooks(form: SearchBooksForm): Promise<void> {
+    this.searchError = '';
+    this.submittingSearch = true;
+
+    try {
+      this.searchResults = await this.bookService.searchBooks(
+        {
+          query: form.query,
+          departmentID: form.departmentID,
+          courseNumber: form.courseNumber,
+        },
+        form.sortID,
+      );
+    } catch (err) {
+      this.searchError = (err as any).toString();
+    }
+
+    this.submittingSearch = false;
   }
 }
